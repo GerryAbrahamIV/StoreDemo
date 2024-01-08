22b59b431c594fd428add6287a7b1bd37db1a7a4
Gerry A <gerry.abraham@idstar.co.id>
2024-01-08 13:24:08 +0700

    remove comment

--- src/main/java/com/example/storedemo2/repository/HumanRepository.java
+++ src/main/java/com/example/storedemo2/repository/HumanRepository.java
@@ -11,13 +11,6 @@ import java.util.List;
 
 @Repository
 public interface HumanRepository extends CrudRepository<Human, Long> {
-/*    @Query("SELECT r.id AS id , r.nama AS nama, r.jenis as jenis, r.alamat as alamat, " +
-            " r.rekening AS rekening, r.created_date AS created_date, r.updated_date as updated_date," +
-            " r.deleted_date as deleted_date, k.id as idKaryawan, k.nama AS namaKaryawan " +
-            " FROM Rekening r " +
-            " JOIN Karyawan k ON k.id = r.id_karyawan "
-            + "WHERE r.id = :id ")
-    RekeningProjection findDetailById(@Param("id") Long id);*/
     @Query("SELECT h FROM Human h WHERE h.id = :id ")
     HumanProjection getById(@Param("id") Long id);
 
